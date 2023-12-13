604fccc6b7dc41b0b86ee819738bcc61cc9a55b3
Sejmou <samo.kolter@gmail.com>
2023-12-13 22:17:58 +0100

    implement track switching

--- src/routes/repertoire/[song]/+page.server.ts
+++ src/routes/repertoire/[song]/+page.server.ts
@@ -1,10 +1,10 @@
-import { s3, generatePresignedUrl, getObjects } from '$lib/server/s3';
+import { s3, generatePresignedUrl, getObjects, getSubpaths } from '$lib/server/s3';
 
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load({ params }) {
 	const songName = params.song;
 	const fileObjects = await getObjects(s3, 'tuwienchor', `mp3s/${songName}`);
-	const tracks = Promise.all(
+	const tracks = await Promise.all(
 		fileObjects.map((object) =>
 			generatePresignedUrl('tuwienchor', object.Key!, 60 * 60).then((url) => {
 				const filename = object.Key!.split('/').pop()!;
@@ -16,5 +16,10 @@ export async function load({ params }) {
 		)
 	);
 
-	return { tracks, songName };
+	const allSongs = await getSubpaths(s3, 'tuwienchor', 'mp3s/');
+	const songIndex = allSongs.indexOf(songName);
+	const nextSong = songIndex < allSongs.length - 1 ? allSongs[songIndex + 1]! : null;
+	const previousSong = songIndex > 0 ? allSongs[songIndex - 1]! : null;
+
+	return { tracks, songName, nextSong, previousSong };
 }
--- src/routes/repertoire/[song]/+page.svelte
+++ src/routes/repertoire/[song]/+page.svelte
@@ -1,21 +1,32 @@
 <script lang="ts">
 	import { createPlaybackStore } from './playback-store';
 	import TrackVolume from './TrackVolume.svelte';
-	import { browser } from '$app/environment';
 	import { get } from 'svelte/store';
+	import { page } from '$app/stores';
+	type PlaybackStore = ReturnType<typeof createPlaybackStore>;
 	export let data;
 
-	let playbackStore: ReturnType<typeof createPlaybackStore>;
+	let song: string | undefined = get(page).params.song;
+	let playbackStore = createPlaybackStore(data.tracks.map((track) => track.url));
 
-	if (browser) {
-		playbackStore = createPlaybackStore(data.tracks.map((track) => track.url));
-	}
+	page.subscribe((value) => {
+		const newSong = value.params.song;
+		console.log(newSong, song);
+		if (newSong != song) {
+			playbackStore.pause();
+			playbackStore = createPlaybackStore(data.tracks.map((track) => track.url));
+		}
+		song = newSong;
+	});
 
 	$: isPlaying = playbackStore.isPlaying;
 	$: currentTime = playbackStore.currentTime;
 	$: duration = playbackStore.duration;
 
-	function handlePlayPause() {
+	$: nextLink = data.nextSong ? `/repertoire/${data.nextSong}` : null;
+	$: previousLink = data.previousSong ? `/repertoire/${data.previousSong}` : null;
+
+	function handlePlayPause(playbackStore: PlaybackStore) {
 		const isPlaying = get(playbackStore.isPlaying);
 		if (isPlaying) {
 			playbackStore.pause();
@@ -30,7 +41,7 @@
 	}
 </script>
 
-<main class="max-w-md mx-auto my-10 p-6 rounded-md shadow-md">
+<main class="w-full max-w-screen-lg mx-auto my-10 p-6 rounded-md shadow-md">
 	<a class="btn mb-2" href="/repertoire">Zur Übersicht</a>
 	<h1 class="text-2xl font-bold mb-4">{data.songName}</h1>
 
@@ -49,23 +60,27 @@
 
 	<div class="flex items-center justify-center space-x-4">
 		<!-- Previous Button with SVG icon -->
-		<button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke="currentColor"
-				class="h-6 w-6"
-			>
-				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
-			</svg>
-		</button>
+		<a href={previousLink}>
+			<button disabled={!previousLink} class="btn">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					class="h-6 w-6"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M15 19l-7-7 7-7"
+					/>
+				</svg>
+			</button>
+		</a>
 
 		<!-- Play Button with SVG icon -->
-		<button
-			class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-			on:click={handlePlayPause}
-		>
+		<button class="btn btn-accent" on:click={() => handlePlayPause(playbackStore)}>
 			{#if $isPlaying}
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
@@ -100,20 +115,24 @@
 		</button>
 
 		<!-- Next Button with SVG icon -->
-		<button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke="currentColor"
-				class="h-6 w-6"
-			>
-				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
-			</svg>
-		</button>
+		<a href={nextLink}>
+			<button disabled={!nextLink} class="btn">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					class="h-6 w-6"
+				>
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+				</svg>
+			</button>
+		</a>
 	</div>
 
-	{#each playbackStore.tracks as track}
-		<TrackVolume {track} />
-	{/each}
+	<div class="grid grid-cols-2 gap-2">
+		{#each playbackStore.tracks as track}
+			<TrackVolume {track} />
+		{/each}
+	</div>
 </main>
